3c37e7c22a68543c110d43dbe265872d6a2699c0
Juan <jsoaricastaneda@gmail.com>
2024-03-15 16:10:42 -0500

    Initial Commit

---
+++ MrBeefApp/MrBeefAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MrBeefAppApp.swift
+//  MrBeefApp
+//
+//  Created by macbook on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MrBeefAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
