812a87b91b42ab76fd14e0bc29fec4ed62022f6b
Juan <jsoaricastaneda@gmail.com>
2024-03-17 13:50:08 -0500

    Se agrego los tab items a la vista principal

--- MrBeefApp/Components/prueba.swift
+++ MrBeefApp/Components/prueba.swift
@@ -9,43 +9,13 @@ import SwiftUI
 
 struct prueba: View {
     
-    @State private var searchText = ""
-       @State private var isEditing = false
+    
     
     var body: some View {
-         HStack {
-             Image(systemName: "magnifyingglass")
-                 .foregroundStyle(.gray)
-             VStack {
-                 TextField("Donde quieres comer hoy?", text: $searchText)
-                     .padding(7)
-                     .padding(.horizontal, 25)
-                     .background(Color(.systemGray6))
-                     .cornerRadius(8)
-                     .padding(.horizontal, 60)
-                     .onTapGesture {
-                         isEditing = true
-                         searchText = ""
-                 }
-             }
-
-             if isEditing {
-                 Button(action: {
-                     isEditing = false
-                     searchText = ""
-                     // Realizar la acción de búsqueda aquí con el valor de searchText
-                 }) {
-                     Text("Cancelar")
-                     
-                         .foregroundStyle(.gray)
-                 }
-                 .padding(.trailing, 10)
-                
-             }
-         }
-     }
- }
-
+        
+        Text("hi")
+    }
+}
 struct prueba_Previews: PreviewProvider {
     static var previews: some View {
         prueba()
--- MrBeefApp/Views/MainView.swift
+++ MrBeefApp/Views/MainView.swift
@@ -12,23 +12,51 @@ struct MainView: View {
         
         ZStack {
             Color(.white).edgesIgnoringSafeArea(.all)
-            VStack {
-                SearchAndFilterBar()
-                
-                Text("LISTA")
-                    .foregroundColor(.gray)
-                GroupImage()
-                    .tabItem{ Label("Explore", systemImage: "magnifyingglass")
+            TabView {
+                ExtractedView()
+                    .tabItem {
+                        Image(systemName: "1.circle")
+                        Text("First")
+                           
                     }
+                    
+                
+                Text("Second tab")
+                    .tabItem {
+                        Image(systemName: "2.circle")
+                        Text("second")
+                    }
+                
+                Text("three tab")
+                    .tabItem {
+                        Image(systemName: "3.circle")
+                        Text("three")
+                    }
+                
+            }
                         
                     }
             }
             
         }
-    }
+    
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
+    }
+}
+
+struct ExtractedView: View {
+    var body: some View {
+        VStack {
+            SearchAndFilterBar()
+            Text("LISTA")
+                .foregroundColor(.gray)
+            GroupImage()
+                .tabItem{Label("Explore", systemImage: "magnifyingglass")
+                    
+                }
+        }
     }
 }
